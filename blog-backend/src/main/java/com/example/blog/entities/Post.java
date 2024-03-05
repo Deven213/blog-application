1695e39f2fa96778b17d10a3e51f6dfc9ce8afe5
deven <devenbhimani123@gmail.com>
2024-03-05 18:15:20 +0530

    initial commit

---
+++ blog-backend/src/main/java/com/example/blog/entities/Post.java
@@ -0,0 +1,62 @@
+package com.example.blog.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+import java.time.LocalDate;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Lob
+    private String content;
+    private LocalDate createdOn;
+
+    public Post() {
+    }
+
+    public Post(String title, String content, LocalDate createdOn) {
+        this.title = title;
+        this.content = content;
+        this.createdOn = createdOn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDate createdOn) {
+        this.createdOn = createdOn;
+    }
+}
