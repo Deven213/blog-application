2508c44a8e6eca3b08c549ef4427291232e7d325
deven <devenbhimani123@gmail.com>
2024-03-06 20:02:42 +0530

    create footer and navbar

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,13 +1,24 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
+import AddPost from "./components/pages/AddPost";
+import ViewBlog from "./components/pages/ViewBlog";
+import UpdatePost from "./components/pages/UpdatePost";
 import Home from "./components/Pages/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" index element={<Home />} />
+          <Route path="/add-blog" exact element={<AddPost />} />
+          <Route path="/blogs/:blogId" exact element={<ViewBlog />} />
+          <Route path="/blogs/:blogId/edit" exact element={<UpdatePost />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
